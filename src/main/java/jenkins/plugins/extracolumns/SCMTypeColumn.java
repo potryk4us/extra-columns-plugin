aeb68a3e2eaaf5d458d2b85677c266abd6863791
Patryk Potrykus <patryk.potrykus@currenda.pl>
2020-05-08 08:20:06 +0200

    Changes

--- src/main/java/jenkins/plugins/extracolumns/SCMTypeColumn.java
+++ src/main/java/jenkins/plugins/extracolumns/SCMTypeColumn.java
@@ -47,7 +47,7 @@ public class SCMTypeColumn extends ListViewColumn {
     public String getScmType(@SuppressWarnings("rawtypes") Job job) {
         if(job instanceof AbstractProject<?, ?>) {
             AbstractProject<?, ?> project = (AbstractProject<?, ?>) job;
-            return project.getScm().getDescriptor().getDisplayName();
+            return project.getScm().getUserRemoteConfigs()[0].getUrl();
         } else {
             String simpleName = job.getClass().getSimpleName();
             if ("WorkflowJob".equals(simpleName)) {

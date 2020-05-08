74fef86a403037177bf94fb351bede67fc65a858
Patryk Potrykus <patryk.potrykus@currenda.pl>
2020-05-08 12:03:53 +0200

    Changes getScm().userRemoteConfigs.toString()

--- src/main/java/jenkins/plugins/extracolumns/SCMTypeColumn.java
+++ src/main/java/jenkins/plugins/extracolumns/SCMTypeColumn.java
@@ -47,7 +47,7 @@ public class SCMTypeColumn extends ListViewColumn {
     public String getScmType(@SuppressWarnings("rawtypes") Job job) {
         if(job instanceof AbstractProject<?, ?>) {
             AbstractProject<?, ?> project = (AbstractProject<?, ?>) job;
-            return project.getScm().getUserRemoteConfigs();
+            return project.getScm().userRemoteConfigs.toString();
         } else {
             String simpleName = job.getClass().getSimpleName();
             if ("WorkflowJob".equals(simpleName)) {
